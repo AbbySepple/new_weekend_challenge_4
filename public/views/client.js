61b76b19526e855e65f734a26a77f007f185d192
Abby Sepple <abby.sepple@gmail.com>
2017-05-07 16:26:30 -0500

    styles

--- public/views/client.js
+++ public/views/client.js
@@ -19,12 +19,14 @@ function getListings(){
 
         if (response[i].rent) {
         var cellTextOne = '<div class="col-md-4 card">';
+        cellTextOne+= "<img src='https://cdn1.iconfinder.com/data/icons/citix/128/city_building_tenement_apartment-512.png'";
         cellTextOne+= "<p>" + response[i].rent+"</p>";
         cellTextOne+= "<p>" + response[i].sqft +"</p>";
         cellTextOne+= "<p>" + response[i].city +"</p>" + '</div>';
         outputDivOne.append(cellTextOne);
       } else {
         var cellTextTwo = '<div class="col-md-4 card">';
+        cellTextTwo+= "<img src='https://cdn.tutsplus.com/psd/uploads/legacy/325_House_Icon/43.jpg'";
         cellTextTwo+= "<p>" + response[i].cost+"</p>";
         cellTextTwo+= "<p>" + response[i].sqft +"</p>";
         cellTextTwo+= "<p>" + response[i].city +"</p>" + '</div>';
